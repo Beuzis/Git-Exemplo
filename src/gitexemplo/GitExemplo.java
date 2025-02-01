9428aaa505f42f181a5112083853c231999af167
Isabela Leite <isabela.1marques@gmail.com>
2025-02-01 10:51:25 -0300

    hello world

--- src/gitexemplo/GitExemplo.java
+++ src/gitexemplo/GitExemplo.java
@@ -4,10 +4,12 @@
  */
 package gitexemplo;
 
-/**
- *
- * @author Isabela
- */
+public class gitexemplo {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+              
 public class GitExemplo {
 
     /**
